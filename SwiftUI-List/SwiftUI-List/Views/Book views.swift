ef057ec9df96bd9f05374ea70c766efd6dfec487
morshedul.alam <morshedul.alam@bjitgroup.com>
2023-02-16 15:52:01 -0500

    BookView implementation has updated.

--- SwiftUI-List/SwiftUI-List/Views/Book views.swift
+++ SwiftUI-List/SwiftUI-List/Views/Book views.swift
@@ -24,17 +24,27 @@ struct TitleAndAuthorStack: View {
  
 extension Book {
     struct Image: View {
+        let image: SwiftUI.Image?
         let title : String
         var size: CGFloat?
+        let cornerRadius: CGFloat
         var body: some View {
-            let symbol = SwiftUI.Image(title: title) ?? .init(systemName: "book")
-            
-            symbol
-                .resizable()
-                .scaledToFit()
-                .frame(width: size, height: size)
-                .font(Font.title.weight(.light))
-                .foregroundColor(.secondary.opacity(0.5))
+            if let image = image {
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: size,height: size)
+                    .cornerRadius(cornerRadius)
+            }else{
+                let symbol = SwiftUI.Image(title: title) ?? .init(systemName: "book")
+                
+                symbol
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: size, height: size)
+                    .font(Font.title.weight(.light))
+                    .foregroundColor(.secondary.opacity(0.5))
+            }
         }
     }
 }
@@ -49,6 +59,13 @@ extension Image{
         }
         
        self.init(systemName: symbolName)
+    }
+}
+
+extension Book.Image{
+    //A preview Image.
+    init(title: String){
+        self.init(image: nil, title: title, cornerRadius: .init())
     }
 }
 
