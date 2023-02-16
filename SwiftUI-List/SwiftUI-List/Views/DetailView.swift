5c1727b5974888a314804a6ac6630ab0c3baa324
morshedul.alam <morshedul.alam@bjitgroup.com>
2023-02-16 11:46:53 -0500

    List View added

---
+++ SwiftUI-List/SwiftUI-List/Views/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  SwiftUI-List
+//
+//  Created by Morshed Alam on 15/2/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    var body: some View {
+        Book.Image(title: book.title)
+        Spacer()
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(book: .init())
+    }
+}
